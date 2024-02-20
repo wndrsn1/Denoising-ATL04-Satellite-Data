0500885eb7dde17acf1796284ed73675a08b454d
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-02-20 15:07:35 -0600

    Update PreprocessH5toPNG.py

--- PreprocessH5toPNG.py
+++ PreprocessH5toPNG.py
@@ -1,56 +1,50 @@
 import os
 import glob
 import h5py
-from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 import zipfile
-datadir2 = '/nfsscratch/Users/wndrsn/2019_atl02'
-datadir4 = '/nfsscratch/Users/wndrsn/2019_atl04'
-save_path = '/nfsscratch/Users/wndrsn/saved_pngs'
-datafiles2 = sorted(glob.glob(os.path.join(datadir2, "*.h5")))
-datafiles4 = sorted(glob.glob(os.path.join(datadir4, "*.h5")))
-if not os.path.exists(save_path):
-  os.makedirs(save_path)
-  print(f"Created directory: {save_path}")
-zip_path = '/Users/wndrsn'
-for i in tqdm(range(4)):
-  file_out = f"{os.path.splitext(os.path.basename(datafiles2[i]))[0]}.png"
-  
-  data = h5py.File(datafiles2[i], 'r')
-  atm = {k: np.array(data[f'/atlas/pce1/atmosphere_s/{k}']) for k in data['/atlas/pce1/atmosphere_s'].keys()}
-  atm_bins = atm['atm_bins']
-  n_records = atm_bins.shape[0]
-  data.close()
-  # def get_atl04():
-  
-  data = h5py.File(datafiles4[i], 'r')
-  nrb = np.array(data['profile_1/nrb_profile'])
-  backg_select = np.array(data['ancillary_data/atmosphere/backg_select'])
-  backg = np.array(data[f'profile_1/backg_method{backg_select[0]}'])
-  range_to_top = np.array(data['profile_1/range_to_top'])
-  z = np.array(data['profile_1/ds_va_bin_h'])
-  nrb_top_bin = np.array(data['profile_1/nrb_top_bin'])
-  nrb_bot_bin = np.array(data['profile_1/nrb_bot_bin'])
-  mol_scatter = np.array(data['meteorology_molec_bkscat/mol_backs_folded'])
-  atm_bins_back = atm_bins-backg[:n_records].reshape(-1, 1)
-  atm_bins_back_mask = np.ma.array(atm_bins_back, mask=atm_bins_back<0)
-  nrb_mask = np.ma.array(nrb, mask=nrb>1e38)
-  adj_z = z[0] - z
-  adj_z = adj_z.reshape(1, -1)
-  range_to_top = range_to_top.reshape(-1, 1)
-  r = range_to_top + adj_z
-  r2 = (r/1000)**2
-  #plt.imshow(atm_bins.T, aspect='auto', cmap='nipy_spectral', vmin=0, vmax=2000)
-  plt.imshow(atm_bins_back.T, aspect='auto', cmap='nipy_spectral', vmin=0, vmax=2000)
-  plt.savefig(os.path.join(save_path,file_out))
-  print(f'Saved {file_out}')
+
+def h5toPNGs():
+  datadir2 = '/nfsscratch/Users/wndrsn/2019_atl02'
+  save_path = '/nfsscratch/Users/wndrsn/saved_pngs'
+  # file = os.path.join(datadir2,'ATL02_20190125212731_04340210_006_01.h5')
+  datafiles2 = glob.glob(os.path.join(datadir2, "*.h5"))
+  zip_path = '/Users/wndrsn'
+
+  if not os.path.exists(save_path):
+    os.makedirs(save_path)
+    print(f"Created directory: {save_path}")
+
+  for file in datafiles2:
+    try:
+      file_out = f"{os.path.splitext(os.path.basename(file))[0]}_vmax50.png"
+      data = h5py.File(file, 'r')
+      atm = {k: np.array(data[f'/atlas/pce1/atmosphere_s/{k}']) for k in data['/atlas/pce1/atmosphere_s'].keys()}
+      atm_bins = atm['atm_bins']
+      n_records = atm_bins.shape[0]
+      data.close()
+
+      
+      atm_mean = atm_bins.mean(axis=1, keepdims=True)
+      atm_bins_mybackg = atm_bins-atm_mean
+
+      #reduce vmax to 25 to get more noise to show up
+      plt.imshow(atm_bins_mybackg.T, aspect='auto', cmap='nipy_spectral', vmin=0, vmax=50)
+      
+
+      plt.savefig(os.path.join(save_path,file_out))
+      print(f'Saved {file_out}')
+    except Exception as e:
+      print(e)  
+
+  # Create a ZIP file containing the saved PNGs using glob
+  zip_filename = 'saved_pngs.zip'
+  with zipfile.ZipFile(zip_filename, 'w') as zipf:
+      for file_path in glob.glob(os.path.join(save_path, "*.png")):
+          zipf.write(file_path, os.path.relpath(file_path, save_path))
+
+  print(f"Saved PNGs compressed to {zip_filename}") 
 
 
-# Create a ZIP file containing the saved PNGs using glob
-zip_filename = 'saved_pngs.zip'
-with zipfile.ZipFile(zip_filename, 'w') as zipf:
-    for file_path in glob.glob(os.path.join(save_path, "*.png")):
-        zipf.write(file_path, os.path.relpath(file_path, save_path))
-
-print(f"Saved PNGs compressed to {zip_filename}")
+h5toPNGs()
