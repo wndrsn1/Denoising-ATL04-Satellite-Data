707e8e77c57e545296b303799a62d5777825b7e0
Will Anderson <146864301+wndrsn1@users.noreply.github.com>
2024-05-17 13:21:12 -0500

    Update Histogram_Background_SNR.py

--- Data_Analysis/Histogram_Background_SNR.py
+++ Data_Analysis/Histogram_Background_SNR.py
@@ -7,14 +7,17 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 def getBins(file):
-    with h5py.File(file, 'r') as data:
-        atm = {k: np.array(data[f'/atlas/pce1/atmosphere_s/{k}']) for k in data['/atlas/pce1/atmosphere_s'].keys()}
-        atm_bins = atm['atm_bins']
-        atm_bins_mybackg = atm_bins 
-        return calculate_SNR(np.array(atm_bins_mybackg))
-    
+    try:
+        with h5py.File(file, 'r') as data:
+            atm = {k: np.array(data[f'/atlas/pce1/atmosphere_s/{k}']) for k in data['/atlas/pce1/atmosphere_s'].keys()}
+            atm_bins = atm['atm_bins']
+            atm_bins_mybackg = atm_bins 
+            return calculate_SNR(np.array(atm_bins_mybackg))
+    except Exception as e:
+        print(e)
+
 def calculate_SNR(dataframe):
-    average_signal = dataframe.mean().mean()
+    average_signal = np.array(dataframe).mean().mean()
     background_signal = average_signal**(1/2)
     SNR = (average_signal-background_signal)/background_signal
     return SNR, background_signal
@@ -26,6 +29,8 @@ def histogram_and_average(atl02_results):
         snr, background_signal = result
         snr_averages.append(snr)
         background_averages.append(background_signal)
+    snr_averages = np.array(snr_averages)
+    background_averages = np.array(background_averages)
     plt.plot(snr_averages)
     plt.title(snr_averages.mean())
     plt.savefig('snr_averages.png')
@@ -37,5 +42,6 @@ datadir2 = '/nfsscratch/Users/wndrsn/atl02'
 datafiles2 = sorted(glob.glob(os.path.join(datadir2, '**/*.h5'), recursive=True))
 with futures.ProcessPoolExecutor() as executor:
     atl02_futures = [executor.submit(getBins, file) for file in datafiles2]   
-    atl02_results = [future.result() for future in tqdm(atl02_futures)]  
+    atl02_results = [future.result() for future in tqdm(atl02_futures)] 
+atl02_results = [atl02 for atl02 in atl02_results if atl02!= None]
 histogram_and_average(atl02_results)
